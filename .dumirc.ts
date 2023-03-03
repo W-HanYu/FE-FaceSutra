515457975649b69f15e3d7d6a01013871b0d53dd
王涛 <wangtao@aimc.online>
2023-03-03 09:33:40 +0800

    FE-FaceSutra

---
+++ .dumirc.ts
@@ -0,0 +1,101 @@
+import { defineConfig } from 'dumi';
+export default defineConfig({
+  favicons: ['./favicon.ico'],
+  base: '/algorithm',
+  publicPath: '/algorithm/',
+  themeConfig: {
+    name: 'Ustinian',
+    title: 'FaceSutra',
+    logo: false,
+    nav: [
+      { title: '面经', link: '/datastr/structure' },
+      {
+        title: '计算机基础',
+        link: '/algorithm/dynamic/subsequence',
+      },
+      { title: '数据结构与算法', link: '/react/bitfield' },
+      { title: '其他部分', link: '/challenge' },
+      {
+        title: 'Github',
+        link: '/',
+      },
+    ],
+    sidebar: {
+      '/algorithm/dynamic': [
+        {
+          title: '动态规划',
+          children: [
+            {
+              link: '/algorithm/dynamic/subsequence',
+              title: '01.不同的子序列',
+              order: 1,
+              frontmatter: { title: '' },
+            },
+            {
+              link: '/algorithm/dynamic/stockTiming',
+              title: '02.买卖股票的最佳时机 III',
+              order: 2,
+              frontmatter: { title: '' },
+            },
+            {
+              link: '/algorithm/dynamic/decoding',
+              title: '03.解码问题',
+              order: 3,
+              frontmatter: { title: '' },
+            },
+          ],
+        },
+        {
+          title: '栈相关',
+          children: [
+            {
+              link: '/algorithm/stack/reversePolish',
+              title: '01.逆波兰表达式求值',
+              order: 1,
+              frontmatter: { title: '' },
+            },
+            {
+              link: '/algorithm/stack/stockTiming',
+              title: '02.基本计算器',
+              order: 2,
+              frontmatter: { title: '' },
+            },
+            {
+              link: '/algorithm/stack/decoding',
+              title: '03.删除相邻重复项',
+              order: 3,
+              frontmatter: { title: '' },
+            },
+          ],
+        },
+        {
+          title: '链表相关',
+          children: [
+            {
+              link: '/algorithm/chainList/reversePolish',
+              title: '01.奇偶链表',
+              order: 1,
+              frontmatter: { title: '' },
+            },
+            {
+              link: '/algorithm/chainList/stockTiming',
+              title: '02.删除链表重复元素II',
+              order: 2,
+              frontmatter: { title: '' },
+            },
+            {
+              link: '/algorithm/chainList/decoding',
+              title: '03.删除链表重复元素',
+              order: 3,
+              frontmatter: { title: '' },
+            },
+          ],
+        },
+      ],
+    },
+    prefersColor: { default: 'dark', switch: true },
+    footer: `Ustinian MIT Licensed | Copyright © 2023-present
+<br />
+Powered by Ustinian`,
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,23 @@
+---
+title: Algorithm
+hero:
+  title: FE-FaceSutra
+  description: A static site based on dumi
+  actions:
+    - text: 开始学习
+      link: /datastr/structure
+    - text: 快来挑战
+      link: /algorithm/dynamic/subsequence
+features:
+  - title: 动画演示
+    emoji: 💎
+    description: 💡💡💡 复杂的算法使用动画讲解的方式，更容易理解
+  - title: 多语言
+    emoji: 🌈
+    description: 🎨🎨🎨 实现方式包括了javascript、typescript、python、Java等常用语言
+  - title: 在线刷题
+    emoji: 🚀
+    description: 无需leecode，在线刷题🔥🔥🔥 免费切好用🔥🔥🔥
+---
+
+FE-FaceSutra
